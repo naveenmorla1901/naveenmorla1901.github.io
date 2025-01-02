46bd906c3e56bd9c9aa35410ac9bacddeadc73ae
naveenmorla1901 <morla.naveen1901@gmail.com>
2025-01-01 23:14:01 -0500

    Fix build configuration for GitHub Actions 3

--- next.config.js
+++ next.config.js
@@ -1,24 +1,12 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  trailingSlash: true,
+  // Remove assetPrefix and basePath for local development
+  assetPrefix: '',
+  basePath: '',
 }
 
 module.exports = nextConfig
